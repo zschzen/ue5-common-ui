28e08440cce8270db68fb146d77d0bac299646f5
Leandro Peres <leandroperes@protonmail.com>
2026-09-10 16:29:08 -0300

    feat(FrontendUI): Add `FrontendPlayerController`

---
+++ Source/FrontendUI/Private/Controllers/FrontendPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2026 Leandro
+
+
+#include "Controllers/FrontendPlayerController.h"
+
---
+++ Source/FrontendUI/Public/Controllers/FrontendPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2026 Leandro
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FrontendPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDUI_API AFrontendPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
